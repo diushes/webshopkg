3c7cc0422fd555ce91fe55a8980df008475befcf
Diushes <diushes1712@gmail.com>
2022-04-03 02:16:06 +0600

    urls fix

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -5,7 +5,7 @@ app_name = "mainpage"
 
 urlpatterns = [
     path("api/shops/", ShopsListApiView.as_view()),
-    path("/", shop_list, name="shop_list"),
-    path("/shops/<int:pk>/", Catalog, name="catalog"),
-    path("/<int:pk>", ShopsByCategory, name="bycategory"),
+    path("", shop_list, name="shop_list"),
+    path("shops/<int:pk>/", Catalog, name="catalog"),
+    path("<int:pk>/", ShopsByCategory, name="bycategory"),
 ]
