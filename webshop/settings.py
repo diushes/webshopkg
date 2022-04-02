c7be99938bb42726516ed8d12cc105e98c9e8427
Diushes <diushes1712@gmail.com>
2022-04-03 00:45:50 +0600

    turned on debug

--- webshop/settings.py
+++ webshop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "3gdn4^nd5*7f#aatcqv&ntld$d-ak#@6+68(y0l6w#30h6!j93"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "*",
