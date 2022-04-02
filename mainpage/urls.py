22a952746901d623e5cc57ec28415861b0bf7731
Diushes <diushes1712@gmail.com>
2022-04-03 02:11:26 +0600

    changed urls

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -4,8 +4,8 @@ from .views import *
 app_name = "mainpage"
 
 urlpatterns = [
-    path("shops/",  ShopsListApiView.as_view()),
-    path("mainpage/shops/<int:pk>/", Catalog, name="catalog"),
-    path("mainpage/", shop_list, name="shop_list"),
-    path('mainpage/<int:pk>', ShopsByCategory, name="bycategory")
+    path("api/shops/", ShopsListApiView.as_view()),
+    path("/", shop_list, name="shop_list"),
+    path("/shops/<int:pk>/", Catalog, name="catalog"),
+    path("/<int:pk>", ShopsByCategory, name="bycategory"),
 ]
